d03f58507b1ec1dd7bd1540a038d17611fa276ec
SnowTheLeopard <mark22dom@gmail.com>
2022-03-31 14:08:18 +0300

    move code

---
+++ contracts/p2pcoinflip/README.md
@@ -0,0 +1,1 @@
+# P2P Coinflip contract
---
+++ contracts/p2pcoinflip/src/lib.rs
@@ -0,0 +1,14 @@
+pub mod commands;
+pub mod contract;
+mod error;
+pub mod msg;
+pub mod queries;
+pub mod state;
+
+pub use crate::error::ContractError;
+
+#[cfg(test)]
+mod testing;
+
+#[cfg(test)]
+mod mock_querier;
